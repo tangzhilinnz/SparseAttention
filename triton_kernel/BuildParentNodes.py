8117149a9f4727ad6ee8ca0254ceebd6099935bd
Tang Zhilin <tangzhilinnz@gmail.com>
2026-01-02 22:14:36 -0600

    Experiment9

--- triton_kernel/BuildParentNodes.py
+++ triton_kernel/BuildParentNodes.py
@@ -1980,7 +1980,7 @@ def run_full_suite_update_X_from_Y():
 
     # 1. Setup Dimensions for Correctness
     # N needs to be a power of 2 usually for easier tree construction logic.
-    B, N, D, H = 32, 256, 64, 16
+    B, N, D, H = 32, 2048 * 4, 64, 16
     dim = H * D
     
     # 2. Initialize Model (Dropout=0.0 for deterministic check)
