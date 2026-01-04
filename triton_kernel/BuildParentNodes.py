a5f4d13bac26a53d0eaae1440914abc5058379a3
Tang Zhilin <tangzhilinnz@gmail.com>
2026-01-03 20:27:06 -0600

    Experiment18

--- triton_kernel/BuildParentNodes.py
+++ triton_kernel/BuildParentNodes.py
@@ -1977,7 +1977,7 @@ def run_full_suite_update_X_from_Y():
     y = torch.randn(B, N - 1, dim, device='cuda', dtype=check_dtype)
     
     # Optional mask (can be None, but good to test with None first for basic sanity)
-    mask = True
+    mask = None
 
     print(f"Input Shapes -> X: {x.shape}, Y: {y.shape}, Dtype: {x.dtype}")
 
