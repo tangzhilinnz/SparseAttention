f7544b37c8dbdf5f9db6b4ee292cc909b792b804
Tang Zhilin <tangzhilinnz@gmail.com>
2026-01-05 14:57:33 -0600

    Experiment27

--- triton_kernel/BuildParentNodes.py
+++ triton_kernel/BuildParentNodes.py
@@ -1656,7 +1656,7 @@ class HierarchicalAttentionFunc(torch.autograd.Function):
         )
 
         
-        CUTOFF_LEVEL = 5
+        CUTOFF_LEVEL = 6
         
         # --- KERNEL A: Low Levels (Split=1) ---
         if LEVELS >= 1:
