d9f7c417d3c7e3d2a153e04dde3f6fa1910ca3b8
Tang Zhilin <tangzhilinnz@gmail.com>
2026-01-06 10:47:01 -0600

    Experiment28

--- triton_kernel/Hierarchical_Attention_Triton.py
+++ triton_kernel/Hierarchical_Attention_Triton.py
@@ -1892,7 +1892,8 @@ def run_full_suite():
     print(f"{'='*60}")
 
     # Config: Massive scale
-    B, N, D, H = 2, 2048 * 64, 512, 8
+    B, N, D, H = 1, 2048 * 128, 512, 8
+    #B, N, D, H = 2, 2048 * 64, 512, 8
     #B, N, D, H = 32, 4096, 512, 8
     #B, N, D, H = 128, 1024, 512, 8 
     #B, N, D, H = 128, 512, 512, 8 
@@ -2084,7 +2085,8 @@ def run_full_suite_update_X_from_Y():
     # Config: Large scale to saturate GPU
     #B, N, D, H = 32, 4096, 64, 8
     #B, N, D, H = 64, 2048, 64, 8
-    B, N, D, H = 2, 2048 * 64, 64, 8
+    #B, N, D, H = 2, 2048 * 64, 64, 8
+    B, N, D, H = 1, 2048 * 128, 64, 8
     #B, N, D, H = 128, 512, 64, 8
     #B, N, D, H = 128, 1024, 64, 8
     dim = D * H
