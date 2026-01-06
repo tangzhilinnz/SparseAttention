0d139b3b2f3d1b9ad8f5fb75f14f28ab8716744f
Tang Zhilin <tangzhilinnz@gmail.com>
2026-01-06 10:49:23 -0600

    Experiment28

--- triton_kernel/Hierarchical_Attention_Triton.py
+++ triton_kernel/Hierarchical_Attention_Triton.py
@@ -1141,7 +1141,7 @@ class HierarchicalAttentionFunc(torch.autograd.Function):
         )
 
         
-        CUTOFF_LEVEL = 9
+        CUTOFF_LEVEL = 10
         
         # --- KERNEL A: Low Levels (Split=1) ---
         if LEVELS >= 1:
