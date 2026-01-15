58cd620caf616362d4b6666457a77744800a7ba9
Tang Zhilin <tangzhilinnz@gmail.com>
2026-01-15 15:51:02 -0600

    Experiment31

--- hierarchical_attention_project/Hierarchical_transformer_Triton_WikiText2.py
+++ hierarchical_attention_project/Hierarchical_transformer_Triton_WikiText2.py
@@ -165,7 +165,7 @@ test_dataset = WikiTextDataset(test_texts, vocab_builder, max_len=MAX_LEN)
 
 # Create dataloaders
 # HYPERPARAMETER: batch_size
-batch_size = 64
+batch_size = 16
 # CHANGED: shuffle=False (Point 3: Maintain continuity)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
 valid_loader = DataLoader(valid_dataset, batch_size=batch_size, drop_last=True)
