5b09b413e4ce5b160f302c7473f8ea6bdabd7649
Tang Zhilin <tangzhilinnz@gmail.com>
2026-01-21 22:32:55 -0600

    Experiment33

--- triton_kernel/Hierarchical_Attention_Triton.py
+++ triton_kernel/Hierarchical_Attention_Triton.py
@@ -2139,7 +2139,7 @@ def run_full_suite_update_X_from_Y():
     # N needs to be a power of 2 usually for easier tree construction logic.
     #B, N, D, H = 1, 2048 * 128, 64, 16
     #B, N, D, H = 16, 2048, 64, 16
-    B, N, D, H = 16, 2048 * 32, 16
+    B, N, D, H = 16, 2048 * 32, 64, 16
     dim = H * D
     
     # 2. Initialize Model (Dropout=0.0 for deterministic check)
@@ -2261,9 +2261,9 @@ def run_full_suite_update_X_from_Y():
     # Config: Large scale to saturate GPU
     #B, N, D, H = 32, 4096, 64, 8
     #B, N, D, H = 64, 2048, 64, 8
-    #B, N, D, H = 2, 2048 * 64, 64, 8
+    B, N, D, H = 2, 2048 * 64, 64, 8
     #B, N, D, H = 1, 2048 * 256, 64, 8
-    B, N, D, H = 128, 512, 64, 8
+    #B, N, D, H = 128, 512, 64, 8
     #B, N, D, H = 128, 256, 64, 8
     #B, N, D, H = 64, 1024, 64, 8
     dim = D * H
