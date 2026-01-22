6206e0459f6fb4f413265b54b566646a2fda5515
Tang Zhilin <tangzhilinnz@gmail.com>
2026-01-22 15:23:45 -0600

    Experiment34

--- triton_kernel/Hierarchical_Attention_Triton.py
+++ triton_kernel/Hierarchical_Attention_Triton.py
@@ -2113,7 +2113,8 @@ def run_full_suite_update_X_from_Y():
     # N needs to be a power of 2 usually for easier tree construction logic.
     #B, N, D, H = 1, 2048 * 128, 64, 16
     #B, N, D, H = 16, 2048, 64, 16
-    B, N, D, H = 2, 2048 * 32, 64, 16
+    #B, N, D, H = 2, 2048 * 32, 64, 16
+    B, N, D, H = 2, 64, 64, 16
     dim = H * D
     
     # 2. Initialize Model (Dropout=0.0 for deterministic check)
