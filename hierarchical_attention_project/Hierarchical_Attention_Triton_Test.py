1fa97aac28b7416f718cb22f9b3380bdffa4e068
Tang Zhilin <tangzhilinnz@gmail.com>
2026-02-19 22:09:10 -0600

    Added unit tests for SWA triton kernels

--- hierarchical_attention_project/Hierarchical_Attention_Triton_Test.py
+++ hierarchical_attention_project/Hierarchical_Attention_Triton_Test.py
@@ -597,10 +597,10 @@ def run_full_suite_update_X_from_Y():
 
     # 1. Setup Dimensions for Correctness
     # B, N, D, H = 1, 2048 * 128, 64, 16
-    # B, N, D, H = 16, 2048, 64, 16
+    B, N, D, H = 16, 2048, 64, 16
     # B, N, D, H = 2, 2048 * 32, 64, 16
     # B, N, D, H = 2, 64, 64, 16
-    B, N, D, H = 16, 256, 64, 16
+    # B, N, D, H = 16, 256, 64, 16
     dim = H * D
     window_size_set = 64
 
